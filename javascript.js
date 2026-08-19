2034fd8818dc8702d36b387b2df84259ed349684
s <sk59466@eid.utexas.edu>
2026-08-19 10:57:21 -0500

    added basic popup

---
+++ javascript.js
@@ -0,0 +1,55 @@
+
+let currentWords = 0;
+let goalWords = 1000;
+const wordCount = document.getElementById("wordCount");
+const percentage = document.getElementById("percentage");
+const wordInput = document.getElementById("wordInput");
+const goalInput = document.getElementById("goalInput");
+const updateWords = document.getElementById("updateWords");
+const updateGoal = document.getElementById("updateGoal");
+const progressBar = document.getElementById("progressBar");
+const pathLength = progressBar.getTotalLength();
+progressBar.style.strokeDasharray = pathLength;
+progressBar.style.strokeDashoffset= pathLength;
+
+function updateUI() {
+    wordCount.textContent = currentWords;
+    const progress = Math.min(currentWords/goalWords, 1);
+    const percent = progress * 100;
+    percentage.textContent = `${percent}%`;
+    const offset = pathLength * (1- progress);
+    progressBar.style.strokeDashoffset = offset;
+}
+updateWords.addEventListener("click", () => {
+    currentWords = Number(wordInput.value);
+    chrome.storage.local.set({
+    currentWords: currentWords,
+});
+    updateUI();
+});
+updateGoal.addEventListener("click", () => {
+    goalWords = Number(goalInput.value);
+     chrome.storage.local.set({
+    goal: goalWords
+});
+    updateUI();  
+});
+updateUI();
+chrome.storage.local.get(
+    ["currentWords", "goal"],
+    (data) => {
+        currentWords = data.currentWords || 0;
+        goalWords = data.goal || 1000;
+        updateUI();
+    }
+);
+
+
+/*const progressBar = document.getElementById("progressBar");
+const pathLength = progressBar.getTotalLength();
+console.log("Path length:", pathLength);
+progressBar.style.strokeDasharray = pathLength;
+progressBar.style.strokeDashoffset = pathLength;
+setTimeout(() => {
+    progressBar.style.strokeDashoffset = pathLength / 2;
+}, 1000);*/
